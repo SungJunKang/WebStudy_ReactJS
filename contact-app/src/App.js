8dc468244bf718cf730ae4c035aea259af2094fc
SungJunKang <std07209@naver.com>
2018-08-22 09:53:11 +0900

    Inserting data into an array

--- contact-app/src/App.js
+++ contact-app/src/App.js
@@ -2,10 +2,26 @@ import React, { Component } from 'react';
 import PhoneForm from './components/PhoneForm';
 
 class App extends Component {
+
+  state = {
+    information: [],
+  }
+
+  handleCreate = (data) => {
+    const {information} = this.state;  
+    this.setState({
+      information: information.concat({
+        ...data,
+        id: this.id++
+      })
+    });
+  }
+  
   render() {
     return (
       <div>
-        <PhoneForm />
+        <PhoneForm onCreate={this.handleCreate} />
+        {JSON.stringify(this.state.information)}
       </div>
     );
   }
