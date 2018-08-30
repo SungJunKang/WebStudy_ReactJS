b47dcd34569048c7a0f363f5e62d4b191a0e5155
SungJunKang <std07209@naver.com>
2018-08-30 09:41:13 +0900

    Choice Option

--- react-tutorial/hello-react/src/components/Contact.js
+++ react-tutorial/hello-react/src/components/Contact.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import ContactInfo from './ContactInfo';
+import ContactDetails from './ContactDetails';
 
 export default class Contact extends Component {
 
     constructor(props){
         super(props);
         this.state = {
+            selectedKey: -1,
             keyword: '',
             contactData: [
                 {name: 'Abet', phone: '010-0000-0001'},
@@ -16,12 +18,21 @@ export default class Contact extends Component {
         };
 
         this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
     }
 
     handleChange(e){
         this.setState({
             keyword: e.target.value
         });
+    }
+
+    handleClick(key){
+        this.setState({
+            selectedKey: key
+        });
+
+        console.log(key, 'is selected');
     }
 
     render() {
@@ -33,7 +44,7 @@ export default class Contact extends Component {
                 }
             );
             return data.map((contact, i) => {
-                return (<ContactInfo contact={contact} key={i}/>)
+                return (<ContactInfo contact={contact} key={i} onClick={() => this.handleClick(i)} />)
             });
         };
 
@@ -42,6 +53,7 @@ export default class Contact extends Component {
                 <h1>Contacts</h1>
                 <input name="keyword" placeholder="Search" value={this.state.keyword} onChange={this.handleChange} />
                 <div>{mapToComponents(this.state.contactData)}</div>
+                <ContactDetails isSelected={this.state.selectedKey != -1} contact={this.state.contactData[this.state.selectedKey]} />
             </div>
         );
     }
