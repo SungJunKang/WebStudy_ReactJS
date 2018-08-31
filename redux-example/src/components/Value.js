d6cc3116917fc26a3e88752e5be0c2d26f1b76f5
SungJunKang <std07209@naver.com>
2018-08-31 11:22:45 +0900

    Redux_Reducer

--- redux-example/src/components/Value.js
+++ redux-example/src/components/Value.js
@@ -1,11 +1,6 @@
 import React, { Component } from 'react';
 
 class Value extends Component {
-
-    constructor(props){
-        super(props);
-    }
-
     render() {
         return (
             <div>
